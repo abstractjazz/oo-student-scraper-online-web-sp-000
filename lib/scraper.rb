e1faa75ab0f273a79042dcb556fc6ef1d70eb9f7
Evan Burton <evan@heyevan.com>
2020-06-10 23:05:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,8 +24,7 @@ class Scraper
       page = Nokogiri::HTML(open(profile_url))
       student = {}
 
-      # student[:profile_quote] = page.css(".profile-quote")
-      # student[:bio] = page.css("div.description-holder p")
+  
       container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
       container.each do |link|
         if link.include?("twitter")
